a0cb6bf5aa14ba9dbd3028389b78ffa3b1aa1363
Kevin Williams Permana <kevinity310@gmail.com>
2023-05-26 19:06:13 +0700

    fix query

--- kevin/scripts/total_order_per_product_monthly.py
+++ kevin/scripts/total_order_per_product_monthly.py
@@ -17,13 +17,12 @@ print("Successfully connected to Snowflake!")
 create_table = """
 CREATE OR REPLACE TABLE total_order_per_product_monthly AS
 SELECT DATE_TRUNC('month', o.order_date) AS ORDER_DATE,
-       p.product_name,
-       SUM(od.unit_price * od.quantity * (1 - od.discount)) AS TOTAL_PRICE
+product_name, SUM(od.quantity) AS TOTAL_ORDER
 FROM orders o
 INNER JOIN order_details od ON o.order_id = od.order_id
 LEFT JOIN products p ON p.product_id = od.product_id
-GROUP BY 1, 2
-ORDER BY 1 ASC
+GROUP BY DATE_TRUNC('month', o.order_date), product_name
+ORDER BY ORDER_DATE, TOTAL_ORDER DESC;
 """
 
 snowflake_cursor.execute(create_table)
@@ -31,7 +30,7 @@ snowflake_conn.commit()
 print("✅ Success to Update Table total_order_per_product_monthly")
 
 create_view = """
-    CREATE OR REPLACE VIEW view_total_order_per_product_monthly AS 
+    CREATE OR REPLACE VIEW VIEW_TOTAL_ORDER_PER_PRODUCT_MONTHLY AS 
         SELECT * FROM total_order_per_product_monthly
     """
 
